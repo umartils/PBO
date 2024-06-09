6fe913fb1215a8375da0e5cc684a91e5e8399be7
kh4l1sh4 <khalisharahman8@gmail.com>
2024-06-09 11:37:51 -0600

    nambahin tampilan

---
+++ UASFIX.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uas.fix;
+
+/**
+ *
+ * @author NekoMorie
+ */
+public class UASFIX {
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
+    
+}
